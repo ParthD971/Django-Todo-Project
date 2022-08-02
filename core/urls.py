1fbb5e14d2eb9e355866de4f8506f8ecc6a1c920
Parth <desaiparth971@gmail.com>
2022-08-02 18:28:31 +0530

    Github social auth completed.

--- core/urls.py
+++ core/urls.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
 from django.urls import path, include
-
+import social_django.urls
 urlpatterns = [
     path('accounts/', include('accounts.urls')),
+    path('oauth/', include('social_django.urls', namespace='social')),
+    path('',  include('todo.urls')),
     path('admin/', admin.site.urls),
 ]
