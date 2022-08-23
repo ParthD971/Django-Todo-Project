814a35d43b4c00f46e7577cbfa024ef139c12ef8
Parth <desaiparth971@gmail.com>
2022-08-23 17:41:45 +0530

    Password reset and forgot password bug fixed.

--- accounts/forms.py
+++ accounts/forms.py
@@ -81,19 +81,24 @@ class ResendActivationCodeForm(forms.Form):
 
 
 class PasswordResetForm(forms.Form):
-    old_password = forms.CharField(required=True)
+    old_password = forms.CharField(required=False)
     password1 = forms.CharField(required=True)
     password2 = forms.CharField(required=True)
 
     def __init__(self, *args, **kwargs):
-        self.user = kwargs.pop('user')
+        self.user = kwargs.pop('user', None)
+        self.reset_password = kwargs.pop('reset_password', None)
         super(PasswordResetForm, self).__init__(*args, **kwargs)
 
     def clean_old_password(self):
-        old_password = self.data.get('old_password')
+        old_password = self.data.get('old_password', None)
 
-        if not self.user.check_password(old_password):
-            self.add_error('old_password', 'Password is incorrect.')
+        if self.reset_password:
+            if not old_password:
+                self.add_error('old_password', 'Password is Required.')
+
+            if not self.user.check_password(old_password):
+                self.add_error('old_password', 'Password is incorrect.')
 
         return old_password
 
