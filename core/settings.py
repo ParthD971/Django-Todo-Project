51f724f6561a4aad81bbe31c5ebf35ef3d14f045
Parth <desaiparth971@gmail.com>
2022-09-02 13:08:22 +0530

    removed comments from settings file

--- core/settings.py
+++ core/settings.py
@@ -173,7 +173,7 @@ LOGIN_URL = 'login'
 LOGOUT_URL = 'logout'
 LOGIN_REDIRECT_URL = 'home'
 LOGOUT_REDIRECT_URL = 'login'
-# LOGIN_ERROR_URL = 'home'
+
 # SOCIAL AUTH SETTINGS
 SOCIAL_AUTH_RAISE_EXCEPTIONS = False
 
@@ -183,8 +183,6 @@ SOCIAL_AUTH_PIPELINE = (
     'social_core.pipeline.social_auth.auth_allowed',
     'social_core.pipeline.social_auth.social_user',
     'social_core.pipeline.user.get_username',
-    # 'social_core.pipeline.mail.mail_validation',
-    # 'social_core.pipeline.social_auth.associate_by_email',
     'social_core.pipeline.user.create_user',
     'social_core.pipeline.social_auth.associate_user',
     'social_core.pipeline.social_auth.load_extra_data',
@@ -209,21 +207,6 @@ SOCIAL_AUTH_FACEBOOK_PROFILE_EXTRA_PARAMS = {
 
 SOCIAL_AUTH_GOOGLE_OAUTH2_KEY = os.environ.get('SOCIAL_AUTH_GOOGLE_OAUTH2_KEY')
 SOCIAL_AUTH_GOOGLE_OAUTH2_SECRET = os.environ.get('SOCIAL_AUTH_GOOGLE_OAUTH2_SECRET')
-
-
-# SOCIAL_AUTH_INSTAGRAM_KEY = os.environ.get('SOCIAL_AUTH_INSTAGRAM_KEY')
-# SOCIAL_AUTH_INSTAGRAM_SECRET = os.environ.get('SOCIAL_AUTH_INSTAGRAM_SECRET')
-#
-# SOCIAL_AUTH_INSTAGRAM_SCOPE = ['email', 'user_link']
-# SOCIAL_AUTH_INSTAGRAM_PROFILE_EXTRA_PARAMS = {
-#     'fields': 'id, name, email'
-# }
-# SOCIAL_AUTH_INSTAGRAM_EXTRA_DATA = [
-#     ('name', 'name'),
-#     ('email', 'email'),
-# ]
-#
-# SOCIAL_AUTH_INSTAGRAM_AUTH_EXTRA_ARGUMENTS = {'scope': 'email user_profile'}
 
 # EMAIL SETTINGS
 EMAIL_USE_TLS = True
